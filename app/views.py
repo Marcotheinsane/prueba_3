acb2163527555b97b94046fb67e5d9b0b81c1f90
marcoTheInsane <bimarcos1888@gmail.com>
2025-12-19 17:19:09 -0300

    forma 1
    pene

--- app/views.py
+++ app/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Curso
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
 
+<<<<<<< Updated upstream
 
+=======
+#crud de cursos
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'curso/listar_cursos.html', {'cursos': cursos})
+def crear_curso(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo')
+        nombre = request.POST.get('nombre')
+        valor = request.POST.get('valor')
+        curso = Curso(codigo=codigo, nombre=nombre, valor=valor)
+        curso.save()
+        return redirect('listar_cursos')
+    return render(request, 'curso/crear_curso.html')
+def editar_curso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    if request.method == 'POST':
+        curso.nombre = request.POST.get('nombre')
+        curso.valor = request.POST.get('valor')
+        curso.save()
+        return redirect('listar_cursos')
+    return render(request, 'curso/editar_curso.html', {'curso': curso})
+def eliminar_curso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    return redirect('listar_cursos')
+>>>>>>> Stashed changes
--- eva3/urls.py
+++ eva3/urls.py
@@ -6,4 +6,8 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('cursos/', views.listar_cursos, name='listar_cursos'),
+    path('cursos/crear/', views.crear_curso, name='crear_curso'),
+    path('cursos/editar/<int:codigo>/', views.editar_curso, name='editar_curso'),
+    path('cursos/eliminar/<int:codigo>/', views.eliminar_curso, name='eliminar_curso'),
 ]
