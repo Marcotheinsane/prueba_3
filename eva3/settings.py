f7ccd04189596522c76810bb16eff95e8e2c7d32
marcoTheInsane <bimarcos1888@gmail.com>
2025-12-19 16:53:13 -0300

    echo
    12lhi12o2hij

--- eva3/settings.py
+++ eva3/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'eva3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evaluacion3',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
