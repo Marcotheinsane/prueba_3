a176979bae08f9392318de588dfd901793d1df9a
ItsMitsuki <mitsukasimoto@gmail.com>
2025-12-19 17:31:16 -0300

    push
    dsadasd

--- app/views.py
+++ app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import redirect, render
-from .models import Curso
+from .models import Curso, Sucursal
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
 
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
 #crud de cursos
 def listar_cursos(request):
     cursos = Curso.objects.all()
     return render(request, 'curso/listar_cursos.html', {'cursos': cursos})
+
 def crear_curso(request):
     if request.method == 'POST':
         codigo = request.POST.get('codigo')
@@ -21,6 +25,7 @@ def crear_curso(request):
         curso.save()
         return redirect('listar_cursos')
     return render(request, 'curso/crear_curso.html')
+
 def editar_curso(request, codigo):
     curso = Curso.objects.get(codigo=codigo)
     if request.method == 'POST':
@@ -29,8 +34,39 @@ def editar_curso(request, codigo):
         curso.save()
         return redirect('listar_cursos')
     return render(request, 'curso/editar_curso.html', {'curso': curso})
+
 def eliminar_curso(request, codigo):
     curso = Curso.objects.get(codigo=codigo)
     curso.delete()
     return redirect('listar_cursos')
 
+<<<<<<< Updated upstream
+=======
+def listar_sucursal(request):
+    sucursales = Sucursal.objects.all()
+    return render(request, 'sucursal/listar_sucursales.html', {'sucursales': sucursales})
+
+def crear_sucursal(request):
+    if request.method == 'POST':
+        codigo = request.POST.get('codigo')
+        nombre = request.POST.get('nombre')
+        ciudad = request.POST.get('ciudad')
+        sucursal = Sucursal(codigo=codigo, nombre=nombre, ciudad=ciudad)
+        sucursal.save()
+        return redirect('listar_sucursales')
+    return render(request, 'sucursal/crear_sucursal.html')
+
+def editar_sucursal(request, codigo):
+    sucursal = Sucursal.objects.get(codigo=codigo)
+    if request.method == 'POST':
+        sucursal.nombre = request.POST.get('nombre')
+        sucursal.ciudad = request.POST.get('ciudad')
+        sucursal.save()
+        return redirect('listar_sucursales')
+    return render(request, 'sucursal/editar_sucursal.html', {'sucursal': sucursal})
+
+def eliminar_sucursal(request, codigo):
+    sucursal = Sucursal.objects.get(codigo=codigo)
+    sucursal.delete()
+    return redirect('listar_sucursales')
+>>>>>>> Stashed changes
--- eva3/urls.py
+++ eva3/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('cursos/crear/', views.crear_curso, name='crear_curso'),
     path('cursos/editar/<int:codigo>/', views.editar_curso, name='editar_curso'),
     path('cursos/eliminar/<int:codigo>/', views.eliminar_curso, name='eliminar_curso'),
+    
+    path('sucursales/', views.listar_sucursal, name='listar_sucursales'),
+    path('sucursales/crear/', views.crear_sucursal, name='crear_sucursal'),
+    path('sucursales/editar/<int:codigo>/', views.editar_sucursal, name='editar_sucursal'),
+    path('sucursales/eliminar/<int:codigo>/', views.eliminar_sucursal, name='eliminar_sucursal'),
 ]
